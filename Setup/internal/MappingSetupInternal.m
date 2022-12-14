fc6b30ea1626432ad654f3e02a4be8db0d57d7cd
Robert Ravier <rravier@math.duke.edu>
2022-12-13 22:49:32 -0700

    added slash check for input paths

--- Setup/internal/MappingSetupInternal.m
+++ Setup/internal/MappingSetupInternal.m
@@ -1,6 +1,15 @@
 %% DO NOT EDIT; SETS PATHS AND GUARANTEES PARAMETERS AS REQUIRED
 
 %Set and make relevant paths
+if isempty(dataPath) || isempty(projectDir)
+    error('Both dataPath and projectDir must be a valid directory')
+end
+if ~(strcmp(dataPath(end),'/') || strcmp(dataPath(end),'\'))
+    dataPath = [dataPath '/'];
+end
+if ~(strcmp(projectDir(end),'/') || strcmp(projectDir(end),'\'))
+    projectDir = [projectDir '/'];
+end
 workingPath = [projectDir specimenGroup '/'];
 touch([workingPath '\MappingData\']);
 rmpath(genpath([SAMSPath 'Alignment']));
@@ -11,7 +20,7 @@ path(path, genpath([SAMSPath 'VisualizationScripts']));
 rmpath(genpath([SAMSPath 'Mapping/external']));  %this is only called for spheres
 
 %Sanity check for parameters, fix if needed
-nullFaceBound = 1e-10;
+nullFaceBound = 1e-16;
 ConfMaxLocalWidth = max(1,ceil(ConfMaxLocalWidth));
 GaussMaxLocalWidth = max(1,ceil(GaussMaxLocalWidth));
 MeanMinLocalWidth = max(1,ceil(MeanMinLocalWidth));
